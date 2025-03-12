e49ca161c99fa07707c611a1a7e593e033ef64b3
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2025-03-13 01:42:56 +0530

    add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // Ensure correct asset paths
+  base: "/",  // Ensure correct asset paths
   build: {
     outDir: "dist",
     assetsDir: "assets",
