1f6e41e3b9f7761a7eedc84faaa18e280644046a
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2025-03-13 00:15:16 +0530

    build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,12 @@ import { BrowserRouter } from "react-router";
 import './index.css'
 import React from 'react'
 import App from './App.jsx'
-window.process = {
-  env: {
-    REACT_APP_API_URL: 'http://localhost:5000',
-    // REACT_APP_API_URL: 'https://color-prediction-nine.vercel.app',
-  },
-};
+// window.process = {
+//   env: {
+//     REACT_APP_API_URL: 'http://localhost:5000',
+//     // REACT_APP_API_URL: 'https://color-prediction-nine.vercel.app',
+//   },
+// };
 
 createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react() ,  tailwindcss()],
+
+//   resolve: {
+//     alias: {
+//       "@mui/icons-material": "@mui/icons-material/esm",
+//     },
+//   },
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react() ,  tailwindcss()],
-
-  resolve: {
-    alias: {
-      "@mui/icons-material": "@mui/icons-material/esm",
-    },
-  },
-})
+  plugins: [react()],
+  base: './', // 👈 Fixes asset paths for Vercel
+});
